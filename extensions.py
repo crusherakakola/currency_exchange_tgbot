5a89024e5ae8a0bf90e4dee2b0539f1d9ffe14c8
crusherakakola <121434396+crusherakakola@users.noreply.github.com>
2023-05-15 19:44:00 +0400

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -17,6 +17,5 @@ class Request:
         url = f"https://api.apilayer.com/currency_data/convert?to={self._quote}&from={self._base}&amount={self._amount}"
         headers = {"apikey": api}
         response = requests.request("GET", url, headers=headers)
-        status_code = response.status_code
         result = json.loads(response.text).get('result')
         return result
